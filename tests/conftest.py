be8017000fc21bd088b4f40d43e2117faf9c2a4b
Mergen <s.mergen@yandex.ru>
2019-10-23 23:24:57 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,23 +9,15 @@ class Api:
     _select_even_url = '/select_even_numbers'
     _get_random_url = '/get_random_even'
     
-    def get_random_even(self, number_range):
-        r = requests.get(f'{self._base_url}{self._get_random_url}/{number_range}')
+    def get_random_even(self, min, max):
+        r = requests.get(f'{self._base_url}{self._get_random_url}?min={min}&max={max}')
 
         return r.json()
     
     def check_is_even(self, number):
         r = requests.get(f'{self._base_url}{self._check_one_url}/{number}')
 
-        return r.json()['result']
-
-    def select_even(self, arr):
-        headers = {'Content-Type': 'application/json'}
-        params = json.dumps(arr)
-        
-        r = requests.post(f'{self._base_url}{self._select_even_url}', params=params, headers=headers)
-        print(r.text)
-        return r.text
+        return r.json()
 
 
 @pytest.fixture
--- tests/test_even_numbers.py
+++ tests/test_even_numbers.py
@@ -2,13 +2,14 @@ import pytest
 
 
 def test_random_even(api):
-    random_even = api.get_random_even(101)
+    random_even = api.get_random_even(2, 1337)
     
-    assert int(random_even) % 2 == 0
+    assert random_even['status'] == 200
+    assert random_even['body']['result'] % 2 == 0
 
 @pytest.mark.parametrize('number, expected', [(10, True), (9, False)])
 def test_is_even(api, number, expected):
     is_even = api.check_is_even(number)
 
-    assert is_even is expected
-
+    assert is_even['status'] == 200
+    assert is_even['body']['result'] is expected
